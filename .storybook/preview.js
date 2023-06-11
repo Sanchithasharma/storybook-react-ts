6a443e2a973b1175ce21c50161cb128020b9f67e
Sanchithasharma <sanchitha.s.r@gmail.com>
2023-06-11 20:29:57 +0930

    added error fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,3 @@
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 
 const customViewports = {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { theme } from "../public/theme";
+import { theme } from "./theme";
 import { ThemeProvider } from "@mui/material/styles";
 
 function App() {
